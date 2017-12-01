c29b11e086e5ac85f617992b67d3460c59faa263
Nesterov Alexey <vertigra.developer@gmail.com>
2017-12-01 19:43:24 +1000

    Small changes

--- NetTelegraph/Type/Account.cs
+++ NetTelegraph/Type/Account.cs
@@ -1,5 +1,9 @@
 ﻿namespace NetTelegraph.Type
 {
+    /// <summary>
+    /// This object represents a Telegraph account.
+    /// See <see href="http://telegra.ph/api#Account">API</see>
+    /// </summary>
     public class Account
     {
     }
