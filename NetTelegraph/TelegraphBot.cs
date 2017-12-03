c81b417c270869d8cd22b95f57c2af52f435e3cb
Nesterov Alexey <vertigra.developer@gmail.com>
2017-12-04 01:00:44 +1000

    Added: method CreateAccount.

--- NetTelegraph/TelegraphBot.cs
+++ NetTelegraph/TelegraphBot.cs
@@ -1,4 +1,7 @@
-﻿using NetTelegraph.Result;
+﻿using System;
+using System.Net;
+using NetTelegraph.Result;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace NetTelegraph
@@ -8,12 +11,51 @@ namespace NetTelegraph
     /// </summary>
     public class TelegraphBot
     {
-        RestClient mRestClient = new RestClient("https://api.telegra.ph");
+        private readonly RestClient mRestClient = new RestClient("https://api.telegra.ph");
 
-        public AccountResult CreateAccount()
+        private const string mCreateAccountUri = "/createAccount";
+
+        private RestRequest NewRestRequest(string uri)
         {
+            return new RestRequest(string.Format(uri), Method.POST);
+        }
 
-            return null;
+        /// <summary>
+        /// Use this method to create a new Telegraph account.
+        /// Most users only need one account, but this can be useful for channel administrators
+        /// who would like to keep individual author names and profile links for each of their channels.
+        /// </summary>
+        /// <param name="shortName">Required. Account name, helps users with several accounts remember which they are currently using. Displayed to the user above the "Edit/Publish" button on Telegra.ph, other users don't see this name.</param>
+        /// <param name="authorName">Default author name used when creating new articles.</param>
+        /// <param name="authorUrl">Default profile link, opened when users click on the author's name below the title. Can be any link, not necessarily to a Telegram profile or channel.</param>
+        /// <returns>
+        /// On success, returns an Account object with the regular fields and an additional access_token field.
+        /// </returns>
+        public AccountResult CreateAccount(string shortName, string authorName = null, string authorUrl = null)
+        {
+            RestRequest request = NewRestRequest(mCreateAccountUri);
+
+            request.AddParameter("short_name", authorName);
+
+            if (authorName != null)
+                request.AddParameter("", authorName);
+            if (authorUrl != null)
+                request.AddParameter("", authorUrl);
+
+            return ExecuteRequest<AccountResult>(request) as AccountResult;
+        }
+
+        private object ExecuteRequest<T>(IRestRequest request) where T : class
+        {
+            IRestResponse response = mRestClient.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                if (typeof (T) == typeof (AccountResult))
+                    return JsonConvert.DeserializeObject<AccountResult>(response.Content);
+            }
+
+            throw new Exception(response.StatusDescription);
         }
     }
 }
