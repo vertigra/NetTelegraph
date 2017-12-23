f988f4f40f8f9ff2a1b6da47a626f3ab88803f68
Nesterov Alexey <vertigra.developer@gmail.com>
2017-12-24 00:49:40 +1000

    Added: enum attrs

---
+++ NetTelegraph/Enum/Attrs.cs
@@ -0,0 +1,19 @@
+﻿namespace NetTelegraph.Enum
+{
+    /// <summary>
+    /// Attributes of the DOM element. 
+    /// Key of object represents name of attribute, value represents value of attribute
+    /// </summary>
+    public enum Attrs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        href,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        src
+    }
+}
--- NetTelegraph/Type/NodeElements.cs
+++ NetTelegraph/Type/NodeElements.cs
@@ -1,26 +1,23 @@
-﻿namespace NetTelegraph.Type
+﻿using NetTelegraph.Enum;
+
+namespace NetTelegraph.Type
 {
     /// <summary>
-    /// 
+    /// This object represents a DOM element node.
     /// </summary>
     public class NodeElements
     {
-        
-        //todo use enum ?
         /// <summary>
         /// Name of the DOM element. 
-        /// Available tags: a, aside, b, blockquote, br, code, em, figcaption, figure, h3, h4, hr, i, iframe, img, li, ol, p, pre, s, strong, u, ul, video.
         /// </summary>
-        public string Tag { get; set; }
+        public Tag Tag { get; set; }
 
-        //todo check use enum ?
         /// <summary>
         /// Optional. 
         /// Attributes of the DOM element. 
         /// Key of object represents name of attribute, value represents value of attribute. 
-        /// Available attributes: href, src.
         /// </summary>
-        public object Attrs { get; set; }
+        public Attrs Attrs { get; set; }
 
         /// <summary>
         /// Optional. List of child nodes for the DOM element.
